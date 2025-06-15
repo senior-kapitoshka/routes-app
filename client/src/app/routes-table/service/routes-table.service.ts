a664989663c11d1b834368b1c6fe56cdc32196a2
sega <senior.kapitoshka@gmail.com>
2025-06-15 16:04:35 +0500

    first

--- client/src/app/routes-table/service/routes-table.service.ts
+++ client/src/app/routes-table/service/routes-table.service.ts
@@ -12,7 +12,7 @@ export class RoutesTableService {
   constructor(private http: HttpClient) { }
 
   getRoutes(): Observable<Route[]> {
-    return this.http.get<Route[]>(`${environment.apiURL}/routes`).pipe(
+    return this.http.get<Route[]>('/api/routes').pipe(
       tap((data: Route[]) => data),
       catchError(err => throwError(() => err))
    )
