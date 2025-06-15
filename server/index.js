6accbf8a94add41c22e011c664db902aa47a117f
sega <senior.kapitoshka@gmail.com>
2025-06-15 15:34:29 +0500

    first

--- server/index.js
+++ server/index.js
@@ -8,13 +8,13 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use(express.static(path.join(__dirname, 'client/dist/routes-app/browser')));
+app.use(express.static(path.join(__dirname, 'client/dist/routes-app')));
 
 app.get('*', (req, res) => {
   if (req.path.startsWith('/api')) {
     return res.status(404).send('API endpoint not found');
   }
-  res.sendFile(path.join(__dirname, 'client/dist/routes-app/browser/index.html'));
+  res.sendFile(path.join(__dirname, 'client/dist/routes-app/index.html'));
 });
 
 
